745df8242299966ac3d58ab74233933cbbcc00bd
webharry <1612677524@qq.com>
2019-05-16 17:17:00 +0800

    up

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import ToastPlugin from "@/components/toast";
+import ToastPlugin from "../dist/toast.umd.min.js";
 
 Vue.config.productionTip = false;
 Vue.use(ToastPlugin);
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pages: {
+      index: {
+          entry: "examples/main.js",
+          template: "public/index.html",
+          filename: "index.html"
+      }
+  },
+  css: {//css前置内联
+    extract: false
+  }
 };
